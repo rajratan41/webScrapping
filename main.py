e4738918000bb984a833231b2e7d455746df56a6
Raj Ratan <110181177+rajratan41@users.noreply.github.com>
2023-05-21 09:55:51 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,86 @@
+# if you want to scrap a website
+# 1. Use the API
+# 2. HTML Web Scraping using some tool like bs4 (Beautiful Soup)
+
+# STEP 1 - Install all the requirements
+# pip install requests
+# pip install bs4
+# pip install html5lib
+
+# Step 2 - Get the HTML
+import requests
+from bs4 import BeautifulSoup
+url = 'https://codewithharry.com/'
+
+r = requests.get(url)
+htmlContent = r.content
+# print(htmlContent)
+
+# Step 3: Parse the HTML
+soup = BeautifulSoup(htmlContent, 'html.parser')
+# print(soup.prettify)
+
+# Step 4: HTML Tree Transversal
+
+
+# Commonly used types of objects:
+# 1. Tag -     print(type(title))
+# 2. NavigableString -  print(type(title.string))
+# 3. BeautifulSoup -  print(type(soup))
+# 4. Comment
+markup = "<p><!-- this is comment --></p>"
+soup2 = BeautifulSoup(markup)
+# print(soup2)
+# print(soup2.p.string)
+# print(type(soup2.p.string))
+# exit()
+
+# Get the Title of the HTML Page
+title = soup.title
+
+# Get all the Paragraphs from the Page
+paras = soup.find_all('p')
+# print(paras)
+
+# Get all the Anchor Tags from the Page
+anchors = soup.find_all('a')
+# print(anchors)
+
+# Get all the links on the Page:
+for link in anchors:
+    h = link.get('href')
+    # print(h)
+
+# Get all links without # and add specific link to it
+# 1. get anchor tag
+anchors = soup.find_all('a')
+all_links = set()
+
+# 2 get all the links
+for link in anchors:
+    if(link.get('href') != '#'):
+        linkText = "https://codewithharry.com" + link.get('href')
+        all_links.add(link)
+        # print(linkText)
+
+# Get the First Element in the HTML page
+fpara = soup.find('p')
+# print(fpara)
+
+# Get classess of any element in the HTML Page
+fpara = soup.find('p')['class']
+# print(fpara)
+
+# Find all the elements with class name text-sm [Finding Specific Class inside Element]
+clas = soup.find_all("p", class_="text-sm")
+# print(clas)
+
+# Get the Text from the tags/soup
+tex = soup.find('p').get_text()
+# print(tex)
+
+# Get text from whole HTML page
+tex2 = soup.get_text()
+# print(tex2)
+
+
